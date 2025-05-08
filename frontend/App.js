eb904f137645b4a5842a9e82c760a9a26a56e865
isulnethila <139636196+isulnethila@users.noreply.github.com>
2025-05-08 11:18:35 +0530

    re create the files

---
+++ frontend/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import StartupScreen from './pages/Startup';
+import HomeScreen from './pages/Home';
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Startup" screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="Startup" component={StartupScreen} />
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
