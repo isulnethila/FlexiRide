ab9191fe8659857b347e3c68fe817ef31df05e4a
isulnethila <139636196+isulnethila@users.noreply.github.com>
2025-05-18 16:29:55 +0530

    Create AddNewVehical.js

---
+++ frontend/pages/AddNewVehical.js
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function AddNewVehical() {
+  return (
+    <View>
+      <Text>AddNewVehical</Text>
+    </View>
+  )
+}
