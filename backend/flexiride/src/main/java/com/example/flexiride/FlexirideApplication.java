04b7c8c267e5282979c1007e18294710d378c4c3
isulnethila <139636196+isulnethila@users.noreply.github.com>
2025-05-25 14:40:52 +0530

    create the backend part

---
+++ backend/flexiride/src/main/java/com/example/flexiride/FlexirideApplication.java
@@ -0,0 +1,13 @@
+package com.example.flexiride;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlexirideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlexirideApplication.class, args);
+	}
+
+}
