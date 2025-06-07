accc44ec5e3f088ece2ec7ca4a3be7c5237e5a48
isulnethila <139636196+isulnethila@users.noreply.github.com>
2025-06-07 17:25:33 +0530

    create ScheduleService

--- backend/flexiride/src/main/java/com/example/flexiride/repository/ScheduleRepository.java
+++ backend/flexiride/src/main/java/com/example/flexiride/repository/ScheduleRepository.java
@@ -3,8 +3,9 @@ package com.example.flexiride.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import com.example.flexiride.model.Schedule;
-
+import com.example.flexiride.model.User;
+import java.util.List;
 @Repository
 public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
-    
+    List<Schedule> findByUser(User user);
 }
---
+++ backend/flexiride/src/main/java/com/example/flexiride/service/ScheduleService.java
@@ -0,0 +1,35 @@
+package com.example.flexiride.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.example.flexiride.repository.ScheduleRepository;
+import com.example.flexiride.model.Schedule;
+import com.example.flexiride.model.User;
+
+import java.util.List;
+import java.util.Optional;
+@Service
+public class ScheduleService {
+    @Autowired
+    private ScheduleRepository scheduleRepository;
+
+    public List<Schedule> getAllSchedules(){
+        return scheduleRepository.findAll();
+    }
+
+    public Optional<Schedule> getSchedulesById(Long id){
+        return scheduleRepository.findById(id);
+    }
+
+    public List<Schedule> getSchedulesByUser(User user){
+        return scheduleRepository.findByUser(user);
+    }
+
+
+    public Schedule saveSchedule(Schedule schedule) {
+        return scheduleRepository.save(schedule);
+    }
+
+
+    
+}
